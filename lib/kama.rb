3c07e501a8433641815a857169fccb2e0ecc5ee2
Joe Fredette <jfredett@gmail.com>
2013-03-03 19:44:48 -0500

    implement CSV converter

--- lib/kama.rb
+++ lib/kama.rb
@@ -1,5 +1,33 @@
 require "kama/version"
+require 'forwardable'
+require 'csv'
 
-module Kama
-  # Your code goes here...
+class Kama
+  extend Forwardable
+
+  # @param data Either a AR class, or collection of AR objects.
+  # @return [String] A csv-string containing all the data given (defaulting to #all)
+  def self.dump(data, *args)
+    if data.is_a?(Class)
+      new(data, data.all)
+    else
+      new(data.class, data)
+    end.to_csv(*args)
+  end
+
+  def initialize(klass, data)
+    @klass = klass
+    @data = data
+  end
+  delegate :column_names => :@klass
+
+  def to_csv(io = "")
+    io << CSV.generate do |output|
+      output << column_names
+
+      @data.each do |obj|
+        output << column_names.map { |c| obj.send(c) }
+      end
+    end
+  end
 end
--- lib/kama/version.rb
+++ lib/kama/version.rb
@@ -1,3 +1,3 @@
-module Kama
+class Kama
   VERSION = "0.0.1"
 end
